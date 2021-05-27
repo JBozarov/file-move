d4aaca731965dfb33d9778c446f0c9fb04cf9141
jbozarov <tech@jbozarov.com>
2021-05-27 15:37:58 -0400

    config server enabled

--- src/main/java/io/javabrains/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/io/javabrains/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,14 @@ package io.javabrains.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class DemoApplication {
+public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 
 }
